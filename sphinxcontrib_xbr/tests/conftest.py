d9aac07857e8914e53c4332fe5eb97f3bfcab7f4
Tobias Oberstein <tobias.oberstein@crossbario.com>
2018-04-09 01:09:25 +0200

    moved to cbsh

--- sphinxcontrib_xbr/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-"""
-    pytest config for sphinxcontrib/xbr/tests
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    :copyright: Copyright 2017 by Crossbar.io Technologies GmbH <support@crossbario.com>
-    :license: BSD, see LICENSE for details.
-"""
-
-pytest_plugins = 'sphinx.testing.fixtures'
